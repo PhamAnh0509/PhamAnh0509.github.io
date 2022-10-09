5ccffcf3c103f6a98e868fb6af52dcadaa5f8652
PhamAnh0509 <78267179+PhamAnh0509@users.noreply.github.com>
2022-10-09 10:49:40 +0700

    Add files via upload

---
+++ HDT-JSI14/SS4/app.js
@@ -0,0 +1,41 @@
+// Câu 1: D
+// Câu 2: D
+// Câu 3: B
+// Câu 4: A
+// Câu 5: A
+// Câu 6: C
+// Câu 7: D
+// Câu 8: D
+
+// Câu 9:
+// const arr = [1,2,3,4,5,6,'hh', '9',80,100];
+
+// const removearr = ["hh", "9"];
+// const new_arr = arr.filter(item => !removearr.includes(item));
+
+// console.log(new_arr);
+
+// Câu 11:
+// class People {
+//     constructor(_name, _age, _address) {
+//       this.name = _name;
+//       this.age = _age;
+//       this.address = _address;
+  
+//     }
+// }
+
+// class Student extends People {
+//     constructor(_name, _age, _address, _id, _math, _physical, _chemistry) {
+//       super(_name, _age, _address);
+//       this.id = _id
+//       this.math = _math;
+//       this.physical = _physical;
+//       this.chemistry = _chemistry
+//     }
+// }
+
+// Câu 10
+// var str = " High knowledge, high return ".toLowerCase();
+// var arr = str.split(', ');
+// console.log( arr);
